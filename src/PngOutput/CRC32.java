b63f5dd4eb4261fc235af420890559878405edcf
Kovács István <kovacsi0907@gmail.com>
2023-11-23 22:11:57 +0100

    Added own CRC implementation that works

--- src/PngOutput/CRC32.java
+++ src/PngOutput/CRC32.java
@@ -2,57 +2,88 @@ package PngOutput;
 
 public final class CRC32 {
 
-    private static final int CRC_TABLE_SIZE = 256;
-    private static final int CRC_TABLE_MASK = 0xFF;
-    private static final long POLYNOMIAL = 0xEDB88320L;
+    public static int[] crcTable = makeCrcTable();
 
-    private static final long[] CRC_TABLE = new long[CRC_TABLE_SIZE];
-    private static boolean crcTableComputed = false;
+    private static int[] makeCrcTable()
+    {
+        int c;
+        int[] table = new int[256];
 
-    private CRC32() {
-        throw new AssertionError("This class cannot be instantiated");
-    }
-
-    private static void makeCRCTable() {
-        for (int n = 0; n < CRC_TABLE_SIZE; n++) {
-            long c = (long) n;
+        for (int n = 0; n < 256; n++) {
+            c = n;
             for (int k = 0; k < 8; k++) {
-                if ((c & 1) == 1) {
-                    c = POLYNOMIAL ^ (c >>> 1);
-                } else {
+                if ((c & 1) == 1)
+                    c = 0xedb88320 ^ (c >>> 1);
+                else
                     c = c >>> 1;
-                }
             }
-            CRC_TABLE[n] = c;
+            table[n] = c;
         }
-        crcTableComputed = true;
+
+        return table;
     }
 
-    public static byte[] updateCRC(byte[] crc, byte[] buf, int len) {
-        long c1 = crc[0] & 0xFF;
-        long c2 = crc[1] & 0xFF;
-        long c3 = crc[2] & 0xFF;
-        long c4 = crc[3] & 0xFF;
+    private static int update_crc(int crc, byte[] buf)
+    {
+        int c = crc;
 
-        if (!crcTableComputed) {
-            makeCRCTable();
+        for (int n = 0; n < buf.length; n++) {
+            c = crcTable[(c ^ buf[n]) & 0xff] ^ (c >>> 8);
         }
+        return c;
+    }
 
-        for (int i = 0; i < len; i++) {
-            long lookupIndex = (c4 ^ buf[i]) & CRC_TABLE_MASK;
-            long temp = CRC_TABLE[(int) lookupIndex];
-            c4 = (c3 ^ temp) & 0xFF;
-            c3 = (c2 ^ (temp >>> 8)) & 0xFF;
-            c2 = (c1 ^ (temp >>> 16)) & 0xFF;
-            c1 = (POLYNOMIAL ^ (temp >>> 24)) & 0xFF;
-        }
-
-        return new byte[]{(byte) c1, (byte) c2, (byte) c3, (byte) c4};
+    public static int crc(byte[] buf)
+    {
+        return update_crc(0xffffffff, buf) ^ 0xffffffff;
     }
 
     public static byte[] calculateCRC(byte[] buf) {
-        byte[] crc = { (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF };
-        byte[] result = updateCRC(crc, buf, buf.length);
-        return new byte[]{ result[0], result[1], result[2], result[3] };
+        int crc = crc(buf);
+        byte[] result = new byte[4];
+
+        result[0] = (byte) ((crc >> 24) & 0xFF);
+        result[1] = (byte) ((crc >> 16) & 0xFF);
+        result[2] = (byte) ((crc >> 8) & 0xFF);
+        result[3] = (byte) (crc & 0xFF);
+
+        return result;
     }
+
 }
+
+/*public final class CRC32 {
+
+    public static long[] crcTable = makeCrcTable();
+
+    private static long[] makeCrcTable() {
+        long c;
+        long[] table = new long[256];
+
+        for (int n = 0; n < 256; n++) {
+            c = n;
+            for (int k = 0; k < 8; k++) {
+                if ((c & 1) == 1)
+                    c = 0xedb88320L ^ (c >>> 1);
+                else
+                    c = c >>> 1;
+            }
+            table[n] = c;
+        }
+
+        return table;
+    }
+
+    private static long update_crc(long crc, byte[] buf) {
+        long c = crc;
+
+        for (int n = 0; n < buf.length; n++) {
+            c = crcTable[(int) ((c ^ buf[n]) & 0xff)] ^ (c >>> 8);
+        }
+        return c;
+    }
+
+    public static long crc(byte[] buf) {
+        return update_crc(0xffffffffL, buf) ^ 0xffffffffL;
+    }
+}*/
